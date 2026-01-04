db81bf85865c1d477bb694675a8c620d3c80c1a3
fanTaux <farisarinanta11@gmail.com>
2026-01-04 19:21:28 +0700

    debugging training

---
+++ 2debugging.py
@@ -0,0 +1,17 @@
+print("Python Academy office report")
+print("June expenses")
+print("---------------------")
+
+# Purchased a birtthday cake and a new keyboard for Vanessa.
+print("Miscellaneous expenses")
+print(34.00 + 22.44)
+
+# Expenses include staff breakfast, suppliesa, and utitlities.
+print("Week 1 expenses")
+print(42.99 + 123.32 + 208.05)
+
+print("Week 2 expenses")
+print(58.93 + 4.04 +184.12)
+
+print("Week 3 expenses")
+print(81.50 + 22.12 + 371.33)
