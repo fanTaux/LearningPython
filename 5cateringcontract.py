cc185f5588e81fb642591fc40cc986433b71aea9
fanTaux <farisarinanta11@gmail.com>
2026-01-07 18:36:52 +0700

    latihan user input

---
+++ 5cateringcontract.py
@@ -0,0 +1,7 @@
+Katering = (input("Masukkan nama perusahaan katering: "))
+Klien = (input("Masukkan nama klien: "))
+Acara = (input("Masukkan jenis acara: "))
+
+print("Kontrak Katering " + Katering)
+print("Katering kontrak ini dibuat antara " + Katering + " dan " + Klien + "Bersama untuk acara " + Acara + " telah diterapkan persetujuan antara pihak untuk acara yang berkoordinasi untuk Katering")
+
