e2dc7962b33a3db0294978a3b2106f822eab1d4c
fanTaux <farisarinanta11@gmail.com>
2026-01-07 18:27:58 +0700

    Menghapus file 4.userinput.py karena pingin aja

--- 4.userinput.py
+++
@@ -1,7 +0,0 @@
-nama = input("Masukkan nama Anda: ")
-kota = input("Masukkan nama kota: ")
-
-print("Nama kota yang dimasukkan adalah:", kota)
-print("Nama kota yang dimasukkan adalah: " + kota)
-
-print("Halo, " +  nama + " " + kota + " " + "Senang bertemu dengan Anda!")
