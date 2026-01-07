545b38aaf34cfcbe1ad11c371b9d147c8efafeb0
fanTaux <farisarinanta11@gmail.com>
2026-01-07 18:59:41 +0700

    learning arithmetic operator in python

---
+++ 7arithmeticoperator.py
@@ -0,0 +1,25 @@
+# Try: x = 47, y = 100 | x = 10.6, y = 2 | x = -5.0, y = -2.0
+
+x = 47
+y = 100
+
+print("x tambah y hasilnya adalah...")
+print(x + y)
+
+print("x dikurangi y hasilnya adalah...")
+print(x - y)
+
+print("x kali y hasilnya adalah...")
+print(x * y)
+
+print("x pangkat y hasilnya adalah...")
+print(x ** y)
+
+print("x dibagi y hasilnya adalah...")
+print(x / y)
+
+print("x dibagi bulat y hasilnya adalah...")
+print(x // y)
+
+print("x modulo y hasilnya adalah...")
+print(x % y)
