63e560311ef6e704b0751df93b88413368ca6562
fanTaux <farisarinanta11@gmail.com>
2026-01-07 18:28:30 +0700

    Perbaikan learning user input

---
+++ 4userinput.py
@@ -0,0 +1,7 @@
+nama = input("Masukkan nama Anda: ")
+kota = input("Masukkan nama kota: ")
+
+print("Nama kota yang dimasukkan adalah:", kota)
+print("Nama kota yang dimasukkan adalah: " + kota)
+
+print("Halo, " +  nama + " " + kota + " " + "Senang bertemu dengan Anda!")
