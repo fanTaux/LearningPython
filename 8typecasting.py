2b7acedeaf263899355977e19c016724159d5a31
fanTaux <farisarinanta11@gmail.com>
2026-01-08 19:28:53 +0700

    new type casting #2

--- 8typecasting.py
+++ 8typecasting.py
@@ -1,6 +1,27 @@
-new_score = float(input("Masukkan nilai baru: "))
-score = (90 + 80 + 70.5 + new_score) / 3
-print("Nilai rata-rata adalah..." + str(score))
+#new_score = float(input("Masukkan nilai baru: "))
+#score = (90 + 80 + 70.5 + new_score) / 3
+#print("Nilai rata-rata adalah..." + str(score))
 
 print(min(10, 20, 30, -5, 0))
 print(max(10, 20, 30, -5, 0))
+
+#str(x) -> mengubah x menjadi tipe data string
+data1 = 32
+print(str(data1))
+print(type(str(data1)))
+
+#int(x) -< mengubah x menjadi tipe data integer
+data2 = 2.78
+print(int(data2))
+print(type(int(data2)))
+
+#float(x) -> mengubah x menjadi tipe data float
+data3 = "18.213"
+print(float(data3))
+print(type(float(data3)))
+
+#bool(x) -> mengubah x menjadi tipe data boolean
+data4 = "mawar"
+print(bool(data4))
+print(type(bool(data4)))
+
