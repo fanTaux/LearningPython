4d8414300792a0731b6a05326b4e8830fb4c3fd9
fanTaux <farisarinanta11@gmail.com>
2026-01-08 19:17:12 +0700

    new function

--- 8typecasting.py
+++ 8typecasting.py
@@ -1,3 +1,6 @@
 new_score = float(input("Masukkan nilai baru: "))
 score = (90 + 80 + 70.5 + new_score) / 3
 print("Nilai rata-rata adalah..." + str(score))
+
+print(min(10, 20, 30, -5, 0))
+print(max(10, 20, 30, -5, 0))
